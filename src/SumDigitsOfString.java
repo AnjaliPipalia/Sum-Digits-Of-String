5b0b0d8f3f21ea5bdf43de7a2463eba8a7ad42d6
Anjali Pipalia <arp226@njit.edu>
2018-12-19 22:16:17 -0800

    added method for default input

---
+++ src/SumDigitsOfString.java
@@ -0,0 +1,12 @@
+public class SumDigitsOfString {
+
+    // For default string
+    protected long getSumFromDefaultString(String defaultString){
+        long sum = 0;
+        String digitOnlyString = defaultString.replaceAll("[^\\d]", "");
+        for(int i = 0;i<digitOnlyString.length();i++){
+            sum += Character.getNumericValue(digitOnlyString.charAt(i));
+        }
+        return sum;
+    }
+}
