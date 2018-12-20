b8439ef2dfbbeac3cf8debf84f1e210d6ed85bd6
Anjali Pipalia <arp226@njit.edu>
2018-12-19 23:49:12 -0800

    added method for getHexadecimalSumFromString

--- src/SumDigitsOfString.java
+++ src/SumDigitsOfString.java
@@ -4,13 +4,21 @@ public class SumDigitsOfString {
     protected long getSumFromDefaultString(String defaultString) {
         long sum = 0;
         String digitOnlyString = defaultString.replaceAll("[^\\d]", "");
-        for (int i = 0; i < digitOnlyString.length(); i++) {
-            sum += Character.getNumericValue(digitOnlyString.charAt(i));
+        for (char c: digitOnlyString.toCharArray()) {
+            sum += Character.getNumericValue(c);
         }
         return sum;
     }
 
-    public long getHexadecimalSumFromString(String input) {
-        return 0;
+    // For handling hex values when flag -x is specified
+    protected long getHexadecimalSumFromString(String input) {
+        long sum = 0;
+        String hexString = input.replaceAll("[^a-fA-F\\d]", "");
+        for(char c: hexString.toCharArray()){
+            if (c >= 'a' && c <= 'f') sum += c - 'a' + 10;
+            else if (c >= 'A' && c <= 'F') sum += c - 'A' + 10;
+            else if (c >= '0' && c <= '9') sum += c - '0';
+        }
+        return sum;
     }
 }
